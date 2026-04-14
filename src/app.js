25974bce0e478a5a7695a0363a7434ecd1ab3b7f
mandloi15 <mandloikratik600@gmail.com>
2026-04-14 17:41:13 +0530

    feat: add account creation flow

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const cookieParser = require("cookie-parser")
 
-const authRoutes = require("./routes/auth.routes")
-
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
 
+/**
+ * - Routes required
+ */
+const authRoutes = require("./routes/auth.routes")
+const accountRoutes = require("./routes/account.routes")
+
+/**
+ *  - Use Routes
+ */
+
 app.use("/api/auth", authRoutes)
+app.use("/api/accounts", accountRoutes)
 
 module.exports = app
--- src/models/account.model.js
+++ src/models/account.model.js
@@ -8,10 +8,12 @@ const accountSchema = new mongoose.Schema({
         index: true
     },
     status:{
+        type: String,
         enum:{
             values: ["ACTIVE","FROZEN","CLOSED"],
-            message: "Status must be either ACTIVE, FROZEN or CLOSED"
-        }
+            message: "Status must be either ACTIVE, FROZEN or CLOSED",
+        },
+        default: "ACTIVE"
     },
     currency:{
         type:String,
@@ -22,4 +24,8 @@ const accountSchema = new mongoose.Schema({
     timestamps: true
 })
 
-const accountModel = mongoose.model("Account", accountSchema)
+accountSchema.index({user: 1, status:1})
+
+const accountModel = mongoose.model("account", accountSchema)
+
+module.exports = accountModel
